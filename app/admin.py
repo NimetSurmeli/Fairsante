1753bcb87e31501825ee7c73a73abefd05364152
syildiz7 <sumeyra.yildiz@boun.edu.tr>
2021-01-19 17:27:14 +0300

    second sending

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from app.models import Product, Ingredient
 
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Ingredient)
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['brand' , 'pname']
+    list_display_links = ['pname']
+    list_filter = ['brand']
 
+    class Meta:
+        model = Product
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ['name' , 'rate']
+    list_filter = ['rate']
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ class Product (models.Model):
         return self.pname
 
 
-# Create your models here.
+
 class Ingredient (models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
